3e2d95a62084a1988e8c190a36a0a717127b3285
The-Royal-WE <brendan.carroll.92@gmail.com>
2014-10-18 20:08:36 +0100

    Addition of inital direction api use

--- js/mapRender.js
+++ js/mapRender.js
@@ -1,8 +1,30 @@
-var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  map = new Map("mapDiv", {
-    center: [-56.049, 38.485],
-    zoom: 3,
-    basemap: "streets"
-  });
-});
+require([
+        "esri/urlUtils", "esri/map", "esri/dijit/Directions", 
+        "dojo/parser", 
+        "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dojo/domReady!"
+      ], function(
+        urlUtils, Map, Directions,
+        parser
+      ) {
+        parser.parse();
+        //all requests to route.arcgis.com will proxy to the proxyUrl defined in this object.
+        urlUtils.addProxyRule({
+          urlPrefix: "route.arcgis.com",  
+          proxyUrl: "/sproxy"
+        });
+        urlUtils.addProxyRule({
+          urlPrefix: "traffic.arcgis.com",  
+          proxyUrl: "/sproxy"
+        });
+
+        var map = new Map("map", {
+          basemap: "streets",
+          center:[-98.56,39.82],
+          zoom: 4
+        });
+
+        var directions = new Directions({
+          map: map
+        },"dir");
+        directions.startup();
+      });
